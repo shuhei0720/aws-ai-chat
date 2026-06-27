a21a3e6dc19aaaa2a31626732a87d885ed0c47f3
shuhei0720 <shuhei_0720@icloud.com>
2026-06-27 13:45:06 +0900

    vitestを導入

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // Vitestの設定を追加
+  test: {
+    globals: true, // describe, it, expect などをグローバルで使用可能に
+    environment: "jsdom", // React コンポーネントのテストのため
+    // setupFiles: "./src/setupTests.ts", // テスト実行前のセットアップファイル（オプション）
+  },
+});
