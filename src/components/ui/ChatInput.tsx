c233fd0d777a014541b8f66c8d7d3ce276008ca0
shuhei0720 <shuhei_0720@icloud.com>
2026-06-28 15:46:57 +0900

    chat入力欄をコンポーネント化

---
+++ src/components/ui/ChatInput.tsx
@@ -0,0 +1,44 @@
+import { FaArrowUp } from "react-icons/fa";
+
+export default function ChatInput() {
+  const models = [
+    {
+      id: "global.anthoropic.claude-sonnet-4-5-20250929-v1:0",
+      name: "Claude 4.5 Haiku v1",
+    },
+    {
+      id: "global.anthoropic.claude-sonnet-4-5-20250929-v1:0",
+      name: "Claude Sonnet 4.5 v1",
+    },
+    {
+      id: "apac.amazon.nova-pro-v1:0",
+      name: "Amazon Nova Pro 1.0",
+    },
+  ];
+
+  return (
+    <form>
+      <div className="flex flex-col justify-between gap-4 rounded-2xl border border-gray-200 p-4">
+        <textarea
+          name="message"
+          className="field-sizing-content max-h-80 w-full resize-none overflow-y-auto border-none break-words outline-none"
+          placeholder="質問を入力してください"
+        />
+        <div className="flex justify-end gap-2">
+          <div className="flex items-center justify-center rounded-md text-sm">
+            <select name="model" className="w-full rounded-md p-2">
+              {models.map((model) => (
+                <option key={model.id} value={model.id}>
+                  {model.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <button className="flex items-center justify-center rounded-md bg-emerald-600 p-2 text-white hover:bg-emerald-700 disabled:opacity-50 disabled:hover:bg-emerald-600">
+            <FaArrowUp className="h-5 w-5" />
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+}
